e16acc632a9b0cde9ec6b9c2d582a28051728137
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-07-05 20:25:23 +0530

    added auto update the pwa on available version

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./sw-listener";
 import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
